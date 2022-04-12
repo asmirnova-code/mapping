7a520cd9c170fb9bceee967c73668f1cd409845f
Anastasia Smirnova <nastja.sm87@gmail.com>
2022-04-12 17:00:33 +0300

    Markers from file added

--- Map1.py
+++ Map1.py
@@ -1,10 +1,16 @@
 import folium
+import pandas
+
+data = pandas.read_csv("volcano.csv")
+lat = list(data['Latitude'])
+lon = list(data['Longitude'])
+
 map = folium.Map(location=[59.942012, 30.304139], zoom_start=10, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[59.942012, 30.304139], [59.93275224017821, 30.250921139378118], [59.92575930636877, 30.296494374976415]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="I am here", icon=folium.Icon(color='red')))
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="I am here", icon=folium.Icon(color='red')))
 
 map.add_child(fg)
 
