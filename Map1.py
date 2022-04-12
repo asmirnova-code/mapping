d58fa43263350d5d23710b10d5b26b62ece03ba5
Anastasia Smirnova <nastja.sm87@gmail.com>
2022-04-12 15:58:57 +0300

    Multiple markers added

--- Map1.py
+++ Map1.py
@@ -2,7 +2,10 @@ import folium
 map = folium.Map(location=[59.942012, 30.304139], zoom_start=10, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[59.942012, 30.304139], popup="I am here", icon=folium.Icon(color='green')))
+
+for coordinates in [[59.942012, 30.304139], [59.93275224017821, 30.250921139378118], [59.92575930636877, 30.296494374976415]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="I am here", icon=folium.Icon(color='red')))
+
 map.add_child(fg)
 
 map.save("Map1.html")
